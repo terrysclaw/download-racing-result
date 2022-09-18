1653987e8868cbefdb3a23a6618ef7cf361c978d
Terry Law <terry.sclaw@gmail.com>
2022-09-18 13:07:32 +0800

    update new version

--- download_marksix.py
+++ download_marksix.py
@@ -1,6 +1,7 @@
 import datetime
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import mysql.connector
 
 
@@ -18,63 +19,65 @@ cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
 
-driver = webdriver.Chrome("chromedriver.exe")
+# driver = webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome()
 
 driver.get("http://win.on.cc/ftpdata/marksix/2021.txt")
 
 sleep(3)
 
-content = driver.find_element_by_xpath("/html/body/pre")
+# content = driver.find_element_by_xpath("/html/body/pre")
+elem = driver.find_element(By.TAG_NAME, "pre")
+print(elem)
+
+# lines = content.text.split("\n")
+
+# for line in lines:
+#     columns = line.split(chr(32))
+
+#     draw_no = columns[0][2:4] + "-" + columns[0][4:7]
 
 
-lines = content.text.split("\n")
-
-for line in lines:
-    columns = line.split(chr(32))
-
-    draw_no = columns[0][2:4] + "-" + columns[0][4:7]
-
-
-    try:
-        add_res = ("INSERT INTO MarkSix(DrawNo) VALUES(%(draw_no)s)")
-        data = {
-            'draw_no': draw_no,
-        }
-        cursor.execute(add_res, data)
-        cnx.commit()
-    except:
-        print(draw_no, "existed!")
+#     try:
+#         add_res = ("INSERT INTO MarkSix(DrawNo) VALUES(%(draw_no)s)")
+#         data = {
+#             'draw_no': draw_no,
+#         }
+#         cursor.execute(add_res, data)
+#         cnx.commit()
+#     except:
+#         print(draw_no, "existed!")
 
     
 	
-    update_res = ("UPDATE MarkSix SET "
-        "Date = %(draw_date)s, "
-        "DrawnNo1 = %(drawn_no1)s, "
-        "DrawnNo2 = %(drawn_no2)s, "
-        "DrawnNo3 = %(drawn_no3)s, "
-        "DrawnNo4 = %(drawn_no4)s, "
-        "DrawnNo5 = %(drawn_no5)s, "
-        "DrawnNo6 = %(drawn_no6)s, "
-        "ExtraNum = %(extra_no)s "
-        "WHERE DrawNo = %(draw_no)s")
+#     update_res = ("UPDATE MarkSix SET "
+#         "Date = %(draw_date)s, "
+#         "DrawnNo1 = %(drawn_no1)s, "
+#         "DrawnNo2 = %(drawn_no2)s, "
+#         "DrawnNo3 = %(drawn_no3)s, "
+#         "DrawnNo4 = %(drawn_no4)s, "
+#         "DrawnNo5 = %(drawn_no5)s, "
+#         "DrawnNo6 = %(drawn_no6)s, "
+#         "ExtraNum = %(extra_no)s "
+#         "WHERE DrawNo = %(draw_no)s")
         
-    data = {
-        'draw_date': datetime.datetime.strptime(columns[1], '%Y/%m/%d'),
-        'drawn_no1': int(columns[2]),
-        'drawn_no2': int(columns[3]),
-        'drawn_no3': int(columns[4]),
-        'drawn_no4': int(columns[5]),
-        'drawn_no5': int(columns[6]),
-        'drawn_no6': int(columns[7]),
-        'extra_no': int(columns[8]),
-        'draw_no': draw_no,
-    }
+#     data = {
+#         'draw_date': datetime.datetime.strptime(columns[1], '%Y/%m/%d'),
+#         'drawn_no1': int(columns[2]),
+#         'drawn_no2': int(columns[3]),
+#         'drawn_no3': int(columns[4]),
+#         'drawn_no4': int(columns[5]),
+#         'drawn_no5': int(columns[6]),
+#         'drawn_no6': int(columns[7]),
+#         'extra_no': int(columns[8]),
+#         'draw_no': draw_no,
+#     }
 
-    cursor.execute(update_res, data)
-    cnx.commit()	
+#     cursor.execute(update_res, data)
+#     cnx.commit()	
         
-cursor.close()
-cnx.close()
+# cursor.close()
+# cnx.close()
 
 sleep(3)
 driver.quit()
---
+++ update_marksix.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from datetime import datetime
+
+from sqlalchemy import create_engine, Column, Date, ForeignKey, Integer, String, text, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declared_attr, Session, relationship
+from sqlalchemy.future import select
+
+class Base:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    __table_args__ = {"mysql_engine": "InnoDB"}
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+
+Base = declarative_base(cls=Base)
+
+engine = create_engine('mysql+pymysql://hkjc:Fr5cypE3D%@mysql.ixx.cc:3306/hkjc')
+
+
+
+class Marksix(Base):
+    __tablename__ = "MarkSix"
+
+    DrawNo = Column(String(6))
+    Date = Column(Date)
+    DrawnNo1 = Column(Integer)
+    DrawnNo2 = Column(Integer)
+    DrawnNo3 = Column(Integer)
+    DrawnNo4 = Column(Integer)
+    DrawnNo5 = Column(Integer)
+    DrawnNo6 = Column(Integer)
+    ExtraNum = Column(Integer)
+
+with Session(engine) as session:
+    with open('2022.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            # print(line)
+            cols = line.split("\t")
+
+            marksix = Marksix()
+            # print(cols)
+            if len(cols)==9:
+                marksix.DrawNo = cols[0][2:4]+'-'+cols[0][4:7]
+                marksix.Date = datetime.strptime(cols[1], "%Y/%m/%d")
+
+                marksix.DrawnNo1 = int(cols[2])
+                marksix.DrawnNo2= int(cols[3])
+                marksix.DrawnNo3 = int(cols[4])
+                marksix.DrawnNo4 = int(cols[5])
+                marksix.DrawnNo5 = int(cols[6])
+                marksix.DrawnNo6 = int(cols[7])
+                marksix.ExtraNum = int(cols[8].split('\n')[0])
+            else:
+                marksix.DrawNo = cols[0].split(' ')[0][2:4]+'-'+cols[0].split(' ')[0][4:7]
+                marksix.Date = datetime.strptime(cols[0].split(' ')[1], "%Y/%m/%d")
+
+                marksix.DrawnNo1 = int(cols[1])
+                marksix.DrawnNo2= int(cols[2])
+                marksix.DrawnNo3 = int(cols[3])
+                marksix.DrawnNo4 = int(cols[4])
+                marksix.DrawnNo5 = int(cols[5])
+                marksix.DrawnNo6 = int(cols[6])
+                marksix.ExtraNum = int(cols[7].split('\n')[0])
+
+
+            try:
+                print(marksix.DrawNo, marksix.Date, marksix.DrawnNo1, marksix.DrawnNo2, marksix.DrawnNo3, marksix.DrawnNo4, marksix.DrawnNo5, marksix.DrawnNo6, marksix.ExtraNum)
+                session.add(marksix)
+                session.commit()
+            except:
+                raise
